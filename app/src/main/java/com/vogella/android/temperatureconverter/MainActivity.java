5fcd60b1f88ff9d773761ddf1505a597c9749553
Amber <skyemakable@gmail.com>
2019-03-06 01:07:52 -0600

    Added stuff so RadioButtons couldn't be manually selected. But it works.

--- app/src/main/java/com/vogella/android/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/vogella/android/temperatureconverter/MainActivity.java
@@ -12,41 +12,21 @@ import android.widget.RadioGroup;
 public class MainActivity extends Activity {
     private EditText text;
 
-    boolean celsius = true;
-    RadioGroup radioGroup;
-    RadioButton cel;
-    RadioButton fah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
         text = findViewById(R.id.inputValue);
-        cel = findViewById(R.id.radio0);
-        fah = findViewById(R.id.radio1);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged (RadioGroup radioGroup, int checkedId) {
-                switch (checkedId) {
-                    case R.id.radio0:
-                        celsius = true;
-                        break;
-                    case R.id.radio1:
-                        celsius = false;
-                        break;
-                }
 
-            }
-        });
     }
 
     //this method is called at button click because we assigned the name to the "OnClick" property of the button
     public void onClick(View view) {
+        RadioButton celsiusButton = findViewById(R.id.radio0);
+        RadioButton fahrenheitButton = findViewById(R.id.radio1);
         switch (view.getId()) {
             case R.id.button1:
-                RadioButton celsiusButton = (RadioButton) findViewById(R.id.radio0);
-                RadioButton fahrenheitButton = (RadioButton) findViewById(R.id.radio1);
                 if (text.getText().length() == 0) {
                     Toast.makeText(this, "Please enter a valid number", Toast.LENGTH_LONG).show();
                     return;
@@ -64,4 +44,5 @@ public class MainActivity extends Activity {
                 break;
         }
     }
+
 }
